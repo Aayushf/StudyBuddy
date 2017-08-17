bc0d8a187915505f7c8d911f8cf2a0ebeba70a38
aayushf <aayushf@gmail.com>
2017-08-17 17:22:08 +0530

    Session 3 Fixed Bugs in adding Topics To Subjects

--- app/src/main/java/objects/Topic.kt
+++ app/src/main/java/objects/Topic.kt
@@ -7,7 +7,7 @@ import java.util.*
 /**
  * Created by aayushf on 16/8/17.
  */
-class Topic(var name :String,  var subjectid:Long):RealmObject() {
+open class Topic(var name :String = "Unspecified",  var subjectid:Long = 0):RealmObject() {
     @PrimaryKey
     var id:Long = 0
     init {
